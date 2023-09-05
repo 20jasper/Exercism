c1f62127e180563c105c00157e0673d5d9e046bd
Jacob Asper <jacobasper191@gmail.com>
2023-09-05 01:31:20 -0400

    get rid of category module to please exercism

--- rust/yacht/src/lib.rs
+++ rust/yacht/src/lib.rs
@@ -1,8 +1,39 @@
-pub mod category;
 mod dice;
 
-use category::*;
 use dice::*;
+
+pub enum Category {
+    Ones,
+    Twos,
+    Threes,
+    Fours,
+    Fives,
+    Sixes,
+    FullHouse,
+    FourOfAKind,
+    LittleStraight,
+    BigStraight,
+    Choice,
+    Yacht,
+}
+
+// QUESTION, is it possible to make a subset of an enum? Like if I wanted
+// `category` to only allow `Ones` through `Sixes`
+//
+// QUESTION is there a better way to match enum variants to a value?
+pub fn category_to_die_value(category: Category) -> Result<u8, &'static str> {
+    let value = match category {
+        Category::Ones => 1,
+        Category::Twos => 2,
+        Category::Threes => 3,
+        Category::Fours => 4,
+        Category::Fives => 5,
+        Category::Sixes => 6,
+        _ => return Err("Invalid Category variant passed, please pass Ones through Sixes"),
+    };
+
+    Ok(value)
+}
 
 /// Get score for categories like "Ones" and "Twos", where points are equal to
 /// the value of the die * its frequency
--- rust/yacht/tests/yacht.rs
+++ rust/yacht/tests/yacht.rs
@@ -1,4 +1,3 @@
-use category::*;
 use yacht::*;
 
 #[test]
