162263e64d88e5dcd146b35d9bc152ac18dd8b6e
Jacob Asper <jacobasper191@gmail.com>
2023-09-05 02:08:34 -0400

    give enum value instead of using conversion function

--- rust/yacht/src/lib.rs
+++ rust/yacht/src/lib.rs
@@ -3,52 +3,18 @@ mod dice;
 use dice::*;
 
 pub enum Category {
-    Ones,
-    Twos,
-    Threes,
-    Fours,
-    Fives,
-    Sixes,
+    Ones = 1,
+    Twos = 2,
+    Threes = 3,
+    Fours = 4,
+    Fives = 5,
+    Sixes = 6,
     FullHouse,
     FourOfAKind,
     LittleStraight,
     BigStraight,
     Choice,
     Yacht,
-}
-
-// QUESTION, is it possible to make a subset of an enum? Like if I wanted
-// `category` to only allow `Ones` through `Sixes`
-//
-// QUESTION is there a better way to match enum variants to a value?
-pub fn category_to_die_value(category: Category) -> Result<u8, &'static str> {
-    let value = match category {
-        Category::Ones => 1,
-        Category::Twos => 2,
-        Category::Threes => 3,
-        Category::Fours => 4,
-        Category::Fives => 5,
-        Category::Sixes => 6,
-        _ => return Err("Invalid Category variant passed, please pass Ones through Sixes"),
-    };
-
-    Ok(value)
-}
-
-/// Get score for categories like "Ones" and "Twos", where points are equal to
-/// the value of the die * its frequency
-///
-/// Example
-///
-/// let category = Category::Fives;
-/// let dice = [5_u8; 5];
-///
-/// let actual = get_upper_section_score(category, &dice);
-/// assert_eq!(actual, 25);
-fn get_upper_section_score(category: Category, dice: &Dice) -> Result<u8, &'static str> {
-    let value = category_to_die_value(category)?;
-
-    Ok(value * get_die_frequency(value, dice))
 }
 
 fn get_straight_score(offset: u8, dice: &Dice) -> u8 {
@@ -77,8 +43,8 @@ pub fn score(_dice: Dice, _category: Category) -> u8 {
         | Category::Fours
         | Category::Fives
         | Category::Sixes => {
-            // QUESTION is this appropriate error handling if this should never fail?
-            get_upper_section_score(_category, &_dice).expect("failed to get score")
+            let value = _category as u8;
+            value * get_die_frequency(value, &_dice)
         }
         Category::FullHouse => {
             match (
