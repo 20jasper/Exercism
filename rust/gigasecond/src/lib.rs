d2acf1904c565428e6a4e8ee862d3800d14c919b
Jacob Asper <jacobasper191@gmail.com>
2023-10-22 02:56:24 -0400

    start paralell letter frequency

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,7 +1,23 @@
-use time::ext::NumericalDuration;
-use time::PrimitiveDateTime as DateTime;
+pub fn find_max_average(nums: Vec<i32>, k: i32) -> f64 {
+    let k = k as usize;
+    let sum = nums.iter().take(k).sum::<i32>();
 
-// Returns a DateTime one billion seconds after start.
-pub fn after(start: DateTime) -> DateTime {
-    start + 1e9.seconds()
+    let max = std::iter::once(sum)
+        .chain(
+            nums
+                // create iterator holding tuples of left and right of window
+                .iter()
+                .zip(nums.iter().skip(k))
+                // store sum at each step
+                .scan(sum, |state, (left, right)| {
+                    *state -= left;
+                    *state += right;
+                    Some(*state)
+                }),
+        )
+        // add original state
+        .max()
+        .unwrap();
+
+    max as f64 / k as f64
 }
